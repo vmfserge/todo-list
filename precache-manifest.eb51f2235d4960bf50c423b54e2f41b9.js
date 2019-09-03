db597bd40f4d1109cf06082bbfee171903e61803
vmfserge <support@funcdev.ru>
2019-09-03 07:57:04 +0300

    Updates

---
+++ precache-manifest.eb51f2235d4960bf50c423b54e2f41b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a1f0089f1e8d6d30c90d2ec7f0817b",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f588b1a6410051b381bf",
+    "url": "/todo-list/static/css/main.3fb8b239.chunk.css"
+  },
+  {
+    "revision": "c18f083f3e8577ae1491",
+    "url": "/todo-list/static/js/2.b7a0abee.chunk.js"
+  },
+  {
+    "revision": "f588b1a6410051b381bf",
+    "url": "/todo-list/static/js/main.70ada0ab.chunk.js"
+  },
+  {
+    "revision": "01051ccfd40824580faa",
+    "url": "/todo-list/static/js/runtime~main.e1edf3a7.js"
+  }
+]);
